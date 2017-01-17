4cf1e209cfb98b843ed4bbbf6e78c448fc0fbe17
Magnus Haugsand <haugsand@gmail.com>
2017-01-17 14:11:29 +0100

    Refaktorering

--- src/oppgaver/oppgave6.js
+++ src/oppgaver/oppgave6.js
@@ -1,4 +1,10 @@
-var bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+
+const NVDBAPI = 'https://www.vegvesen.no/nvdb/api/v2';
+
+
+const loadingIndicator = document.querySelector('.loading');
+
+const bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -8,7 +14,7 @@ var bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}
 });
 
 
-var mymap = L.map('mapid', {
+const mymap = L.map('mapid', {
     maxBounds: [[55.86, -0.26], [64.89, 18.50]],
     minZoom: 6,
 });
@@ -18,77 +24,105 @@ bakgrunnsLag.addTo(mymap);
 
 
 
-var vegobjekter = {};
+const vegobjekter = {};
 
 
-var heat = L.heatLayer([], {
+const heat = L.heatLayer([], {
     radius: 10
 }).addTo(mymap);
 
 
-const loadingIndicator = document.querySelector('.loading');
+
+mymap.on('moveend', function () {
+    hentData();
+});
+
+mymap.setView([60.39, 5.33], 16);
+
+
+
+
+
+
+function showLoadingIndicator () {
+    loadingIndicator.style.opacity = 1;
+}
+
+function hideLoadingIndicator () {
+    loadingIndicator.style.opacity = 0;
+}
+
+
+function getStatistics (vegobjekter) {
+
+    let statistikk = {};
+
+    vegobjekter.forEach(vegobjekt => {
+
+        if (statistikk.hasOwnProperty('id')) {
+            statistikk.id.push(vegobjekt.id);
+        } else {
+            statistikk.id = [vegobjekt.id];
+        }
+
+        vegobjekt.egenskaper.forEach(egenskap => {
+
+            if (!statistikk.hasOwnProperty(egenskap.navn)) {
+                statistikk[egenskap.navn] = {};
+            }
+
+            if (statistikk[egenskap.navn].hasOwnProperty(egenskap.verdi)) {
+                statistikk[egenskap.navn][egenskap.verdi].push(vegobjekt.id);
+
+            } else {
+                statistikk[egenskap.navn][egenskap.verdi] = [vegobjekt.id];
+            }
+
+        });
+    });
+
+    return statistikk;
+}
+
+
+
+function addVegobjekter (result) {
+    result.forEach(vegobjekt => {
+
+        if (!vegobjekter.hasOwnProperty(vegobjekt.id)) {
+
+            var wkt = vegobjekt.geometri.wkt;
+            var point = Terraformer.WKT.parse(wkt);
+
+            vegobjekter[vegobjekt.id] = L.marker(point.coordinates);
+
+            heat.addLatLng(point.coordinates);
+
+        }
+    })
+}
+
 
 
 function hentData () {
 
+    const kartutsnitt = mymap.getBounds().toBBoxString();
 
+    const url = NVDBAPI + '/vegobjekter/570.json?inkluder=geometri,egenskaper&srid=wgs84&antall=10000&egenskap="5074!=6431 AND 5055>\'1999-12-31\'"&kartutsnitt=' + kartutsnitt;
 
-    var statistikk = {};
-    var kartutsnitt = mymap.getBounds().toBBoxString();
-
-    var url = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/570.json?inkluder=geometri,egenskaper&srid=wgs84&antall=10000&egenskap="5074!=6431 AND 5055>\'1999-12-31\'"&kartutsnitt=' + kartutsnitt;
-
-    loadingIndicator.style.opacity = 1;
+    showLoadingIndicator();
 
     fetch(url)
         .then(function(response) {
             return response.json()
+
         }).then(function(json) {
 
-            loadingIndicator.style.opacity = 0;
+            hideLoadingIndicator();
 
-            for (var i = 0; i < json.objekter.length; i++) {
+            const statistikk = getStatistics(json.objekter);
 
-
-
-                if (statistikk.hasOwnProperty('id')) {
-                    statistikk.id.push(json.objekter[i].id);
-                } else {
-                    statistikk.id = [json.objekter[i].id];
-                }
-
-                for (var j = 0; j < json.objekter[i].egenskaper.length; j++) {
-                    var egenskap = json.objekter[i].egenskaper[j];
-
-                    if (!statistikk.hasOwnProperty(egenskap.navn)) {
-                        statistikk[egenskap.navn] = {};
-                    }
-
-                    if (statistikk[egenskap.navn].hasOwnProperty(egenskap.verdi)) {
-                        statistikk[egenskap.navn][egenskap.verdi].push(json.objekter[i].id);
-                    } else {
-                        statistikk[egenskap.navn][egenskap.verdi] = [json.objekter[i].id];
-                    }
-
-                }
-
-
-
-                if (!vegobjekter.hasOwnProperty(json.objekter[i].id)) {
-
-
-                    var wkt = json.objekter[i].geometri.wkt;
-                    var point = Terraformer.WKT.parse(wkt);
-
-                    vegobjekter[json.objekter[i].id] = L.marker(point.coordinates);
-
-                    heat.addLatLng(point.coordinates);
-
-
-                }
-
-            }
-
+            addVegobjekter(json.objekter);
 
 
             console.log(statistikk);
@@ -128,11 +162,6 @@ function hentData () {
 
 
 
-mymap.on('moveend', function () {
-    hentData();
-});
-
-mymap.setView([60.39, 5.33], 16);
 
 
 var ctx = document.getElementById("myChart");
@@ -150,7 +179,7 @@ var data = {
     ],
     datasets: [
         {
-            data: [1, 1, 1, 1, 1, 1, 1],
+            data: [0, 0, 0, 0, 0, 0, 0],
             label: "Ukedag",
             backgroundColor: [
                 "#d53e4f",
@@ -181,7 +210,7 @@ var myPieChart = new Chart(ctx,{
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero: true
                 }
             }]
         }

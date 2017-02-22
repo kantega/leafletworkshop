28befd45e9cdd6abf3c17966ee1c5e65b93a879b
Lars Lønne <lars.lonne@kantega.no>
2017-02-22 21:11:21 +0100

    feat(oppgave2): oppgavetekster for oppgave 2

--- src/oppgaver/oppgave2.js
+++ src/oppgaver/oppgave2.js
@@ -1,4 +1,3 @@
-
 const bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -7,77 +6,114 @@ const bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 });
+const duration = 0.5;
 
-const DURATION = 0.5;
+// Avgrensning av kartet for oppgave 2.1
+const boundarySouthWest = [55.86, -0.26];
+const boundaryNorthEash = [64.89, 18.50];
 
-const akvariet = L.polygon([
-    [60.39973, 5.30204], 
-    [60.40063, 5.30399], 
-    [60.40011, 5.30590], 
-    [60.39863, 5.30412]
-], 
-{
-  color: '#0f0'
-});
-akvariet.bindPopup('Akvariet');
+/*
+ Oppgave 2.1 - Avgrens kartet
 
-const bryggen = L.marker([60.3973, 5.3233]);
-bryggen.bindPopup('Bryggen');
-
-var floibanen = L.polyline([
-    [60.3964173561773, 5.328556895256043], 
-    [60.39660550579725, 5.329484939575195], 
-    [60.396311355912495, 5.331035256385803], 
-    [60.39560644537202, 5.333991050720216], 
-    [60.39533878737165, 5.337563753128053], 
-    [60.39480346476893, 5.342504382133484]
-], 
-{
-  color: '#f00'
-});
-floibanen.bindPopup('Fløibanen');
-
-
+ Vi ønsker ikke at det skal være mulig å zoom/flytte kartet for langt. Over har
+ vi gitt dere koordinatene til to punkter, som bestemmer avgrensningen. Bruk
+ options-objektet til L.map for å avgrense kartet. Se
+ http://leafletjs.com/reference-1.0.3.html#map-maxbounds og
+ http://leafletjs.com/reference-1.0.3.html#map-minzoom.
+ */
 const map = L.map('mapid', {
-    maxBounds: [[55.86, -0.26], [64.89, 18.50]],
-    minZoom: 6 
+    // din kode her
 });
-map.setView([60.39, 5.33], 12);
 map.addLayer(bakgrunnsLag);
-map.addLayer(akvariet);
-map.addLayer(bryggen);
-map.addLayer(floibanen);
+map.setView([60.39, 5.33], 12);
+
+/*
+ Oppgave 2.2 - polygon rundt akvariet
+
+ Under har vi gitt dere koordinatene til området som avgrenser Akvariet i
+ Bergen. Bruk L.polygon til å tegne et polygon på kartet. Fargen skal være
+ grønn. Se http://leafletjs.com/reference-1.0.3.html#polygon
+ */
+const akvariet = [
+    [60.39973, 5.30204],
+    [60.40063, 5.30399],
+    [60.40011, 5.30590],
+    [60.39863, 5.30412]
+];
+// din kode her
 
 
+/*
+ Oppgave 2.3 - Markør på Bryggen
+
+ Konstanten bryggen er koordinatene til Bryggen. Vis en markør på Bryggen med
+ L.marker. Se http://leafletjs.com/reference-1.0.3.html#marker
+ */
+const bryggen = [60.3973, 5.3233];
+// din kode her
+
+/*
+ Oppgave 2.4 - Fløibanen
+
+ Bruk L.polyline for å markere traséen til Fløibanen. Koordinatene er gitt
+ under. Se http://leafletjs.com/reference-1.0.3.html#polyline
+ */
+const floibanen = [
+    [60.3964173561773, 5.328556895256043],
+    [60.39660550579725, 5.329484939575195],
+    [60.396311355912495, 5.331035256385803],
+    [60.39560644537202, 5.333991050720216],
+    [60.39533878737165, 5.337563753128053],
+    [60.39480346476893, 5.342504382133484]
+];
+// din kode her
+
+/*
+ Oppgave 2.5 - Popups
+
+ Når man klikker på de forskjellige markørene i kartet, skal det dukke opp en
+ popup som forklarer hva du klikket på. Bruk L.popup for å legge til en markør
+ på Akvariet, Bryggen og Fløibanen. Se
+ http://leafletjs.com/reference-1.0.3.html#popup
+ */
+// din kode her
+
+
+/*
+ Oppgave 2.6 - refaktorisering av navigasjonen
+
+ Alle flyTo-kallene har nå hardkodete koordinater. Skriv dem om til å hente
+ koordinatene fra markørene som vi har laget tidligere, slik at tallene bare er
+ skrevet inn ett sted. Endre også flyTo til flyToBounds der det er aktuelt, slik
+ at Leaflet regner ut zoom-nivå på egen hånd. Se
+ http://leafletjs.com/reference-1.0.3.html#map-flytobounds
+ */
 document.querySelector('.js-akvariet').addEventListener('click', () => {
-    map.flyToBounds(akvariet.getBounds(), {
-        duration: DURATION,
+    map.flyTo([60.399737639725814, 5.304079055786133], 18, {
+        duration
     });
-}, false);
+});
 
 document.querySelector('.js-bryggen').addEventListener('click', () => {
-    map.flyTo(bryggen.getLatLng(), 18, {
-        duration: DURATION,
+    map.flyTo([60.3973, 5.3233], 18, {
+        duration
     });
-}, false);
+});
 
 document.querySelector('.js-floien').addEventListener('click', () => {
-    map.flyToBounds(floibanen.getBounds(), {
-        duration: DURATION,
+    map.flyTo([60.39573099834468, 5.334892272949219], 18, {
+        duration
     });
-}, false);
+});
 
 document.querySelector('.js-helebergen').addEventListener('click', () => {
     map.flyTo([60.39, 5.33], 12, {
-        duration: DURATION,
+        duration
     });
-}, false);
+});
 
 
 function onMapClick(e) {
     console.log('Du klikket på koordinaten [' + e.latlng.lat + ', ' + e.latlng.lng + ']');
 }
-
 map.on('click', onMapClick);
-
-

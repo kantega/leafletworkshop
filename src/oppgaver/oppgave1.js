44360b57444b2fe10591c0f2295d24fff5beaec3
Johannes Fauske <johannfa@gmail.com>
2022-03-01 13:10:02 +0100

    korrekt initialisering av leaflet

--- src/oppgaver/oppgave1.js
+++ src/oppgaver/oppgave1.js
@@ -8,12 +8,12 @@
 
 // bakgrunnsLag definerer kartdataene som vi viser i kartet. Vi har satt det opp
 // for dere, men til andre prosjekter kan dere hente data fra mapbox.com
-const bakgrunnsLag = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+const bakgrunnsLag = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets',
+    id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 });
 
